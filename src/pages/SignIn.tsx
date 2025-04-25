99a7e3ba541211e421da468941bc1db71d36be0f
Mishra <gajendra.mishra7503@gmail.com>
2025-04-25 12:37:57 +0530

    first commit

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -119,6 +119,7 @@ const AuthModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }
                     return toast.error('Check password');
                 }
     
+                
                 const response = await fetch('http://localhost:5000/api/auth/signup', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
@@ -130,6 +131,8 @@ const AuthModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }
                         address: form.address,
                     }),
                 });
+
+
                 const data = await response.json();
     
                 if (!response.ok) throw new Error(data.message || 'Signup failed');
